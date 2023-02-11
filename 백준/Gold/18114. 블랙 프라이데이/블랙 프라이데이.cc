e14c819aad3fa846b91b53b6be174f6e375201ab
SsalDduck <124281401+SsalDduck@users.noreply.github.com>
2023-02-11 11:40:04 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 176 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.cc
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.cc
@@ -1,30 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int N, C;
-int ar[5001];
-vector<int> ps;
-vector<int> dp[5001];
+vector<int> v;
 bool ans;
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	cin >> N >> C;
+	v.resize(N);
 	for (int i = 0; i < N; i++) {
-		cin >> ar[i]; if (ar[i] == C) ans = 1;
+		cin >> v[i]; if (v[i] == C) ans = 1;
 	}
-	sort(ar, ar + N);
+	stable_sort(v.begin(), v.end());
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < i; j++) {
-			dp[i].push_back(ar[i] + ar[j]);
-			if (ar[i] + ar[j] == C)
+			if (binary_search(v.begin() + i + 1, v.end(), C - v[i] - v[j])) 
 				ans = 1;
-		}
-		//sort(dp[i].begin(), dp[i].end());
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j <i; j++) {
-			if(binary_search(dp[j].begin(), dp[j].end(), C - ar[i]))ans=1;
+			if (v[i] + v[j] == C)
+				ans = 1;
 		}
 	}
 	cout << ans;
