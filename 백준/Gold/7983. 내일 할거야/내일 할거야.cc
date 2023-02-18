18196be3923a275586a426246d5fe8c6a0107e07
SsalDduck <124281401+SsalDduck@users.noreply.github.com>
2023-02-18 14:18:14 +0900

    [Gold V] Title: 내일 할거야, Time: 280 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N;
+vector<pair<int, int>> v;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int s, d;
+		cin >> s >> d;
+		v.push_back({ s,d });
+	}
+	sort(v.begin(), v.end(), [](auto a,auto b) {
+		if (a.second > b.second) 
+			return 1; // 앞에놈이 크도록 정렬d
+		return 0;
+		});
+	
+	int ti = INT_MAX;
+	for (int i = 0; i < N; i++) 
+		ti = min(ti - v[i].first, v[i].second - v[i].first);
+	cout << ti;
+}
