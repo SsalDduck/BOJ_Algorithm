bb6538dddfed4782bea17d1a29e4bb2519510b59
SsalDduck <124281401+SsalDduck@users.noreply.github.com>
2023-02-28 16:38:52 +0900

    [Gold V] Title: 개똥벌레, Time: 56 ms, Memory: 5520 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.cc
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.cc
@@ -15,7 +15,7 @@ int main() {
 	sort(v[1].begin(), v[1].end());
 	int ans = INT_MAX,ans2=0;
 	for (int h = 1; h <= H; h++) {
-		a[h] += v[0].size() - (lower_bound(v[0].begin(), v[0].end(),h)-v[0].begin());
+		a[h] += v[0].end() - lower_bound(v[0].begin(), v[0].end(),h);
 		a[h] += v[1].end() - upper_bound(v[1].begin(), v[1].end(),H-h);
 		if (a[h] < ans) {
 			ans2 = 1;
